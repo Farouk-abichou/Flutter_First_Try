f32a6c81f8fcb61bdaad119423c3a97c64a9b6f1
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-01-31 10:16:41 +0100

    First Time with Flutter

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp2/view/notes_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NotesView(),
+    );
+  }
+}
