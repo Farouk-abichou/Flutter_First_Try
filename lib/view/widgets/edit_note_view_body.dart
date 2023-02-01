7fb8d19a1d8cb0f297fd5a3760a568d2ea16f1fe
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-02-01 10:54:35 +0100

    not so weird

--- lib/view/widgets/edit_note_view_body.dart
+++ lib/view/widgets/edit_note_view_body.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp2/view/widgets/custom_app_bar.dart';
+import 'package:flutterapp2/view/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children:  const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(
+            title: 'Edit Note',
+            icon : Icons.check
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          CustomTextField(hint: "Title"),
+          SizedBox(
+            height: 20,
+          ),
+          CustomTextField(
+              hint: "Content",
+              maxLines: 4,
+          ),
+        ],
+      ),
     );
   }
 }
